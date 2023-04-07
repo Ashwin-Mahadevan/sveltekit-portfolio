72be491c0a5cc2f5e2b83a80b1680f4571406dc5
Ashwin Mahadevan <ashwinm@berkeley.edu>
2023-04-07 13:32:46 -0700

    Backup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
+  import { page } from "$app/stores";
 
+  import { inject } from "@vercel/analytics";
   inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
+  {#if $page.data.title}
+    <title>{$page.data.title} | Ashwin Mahadevan</title>
+  {/if}
+
   <title>Ashwin Mahadevan</title>
 </svelte:head>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <h1>Ashwin Mahadevan</h1>
 
-<p>This site is under construction.</p>
+<p>
+  Hey, I'm Ashwin. I'm the Founder and CEO of <a href="https://parasocial.ai/">Parasocial AI</a>,
+  where we're building platforms that foster relationships between humans and machines.
+</p>
 
-<ul>
-  <li>Email: ashwinm@berkeley.edu</li>
-  <li>Twitter: @shwin_m</li>
-</ul>
+<div class="info">
+  <ul>
+    <li>Email: ashwinm@berkeley.edu</li>
+    <li>Twitter: @shwin_m</li>
+  </ul>
+</div>
+
+<style>
+  div.info {
+    flex: 1;
+  }
+</style>
