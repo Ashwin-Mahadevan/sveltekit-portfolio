14110f9d8d43025197e944f9afce2e5f8103d3e1
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-05-15 19:27:21 -0700

    Styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   const links = [
-    { text: "Website: ashwinm.com", href: "https://ashwinm.com" },
-    { text: "Email: ashwinm@berkeley.edu", href: "mailto:ashwinm@berkeley.edu" },
-    { text: "Twitter: @shwin_m", href: "https://twitter.com/shwin_m" }
+    { name: "Website", content: "ashwinm.com", href: "https://ashwinm.com" },
+    { name: "Email", content: "ashwinm@berkeley.edu", href: "mailto:ashwinm@berkeley.edu" }
   ];
 </script>
 
 <div class="my-12 space-y-4">
   <h1 class="text-5xl font-bold text-center">Ashwin Mahadevan</h1>
 
-  <ul class="flex space-x-2">
-    {#each links as { href, text }, index}
-      <li><a {href}> {text} </a></li>
+  <ul class="flex justify-center">
+    {#each links as { name, content, href }, index}
+      <li>{name}: <a {href} class="text-blue-800"> {content} </a></li>
+      {#if index < links.length - 1}
+        <span class="mx-2">|</span>
+      {/if}
     {/each}
   </ul>
 
