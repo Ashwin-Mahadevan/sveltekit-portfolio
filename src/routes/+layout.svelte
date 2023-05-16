427f9e73378f9dec4ba7bc05deba0f8c598548ae
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-05-15 18:42:00 -0700

    Installed Tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
+  import "../app.css";
   import { dev } from "$app/environment";
-  import { page } from "$app/stores";
-
   import { inject } from "@vercel/analytics";
+
   inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
-  {#if $page.data.title}
-    <title>{$page.data.title} | Ashwin Mahadevan</title>
-  {/if}
-
   <title>Ashwin Mahadevan</title>
 </svelte:head>
 
