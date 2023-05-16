ed1e9dbed5562446067bb1ba2b4e1af1b6c1877c
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-05-15 18:49:22 -0700

    Styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
-<h1>Ashwin Mahadevan</h1>
+<div class="my-12 space-y-4">
+  <h1 class="text-5xl font-bold text-center">Ashwin Mahadevan</h1>
 
-<p>Hi, I'm Ashwin. I'm a mathematician and entrepreneur.</p>
+  <p>Hi, I'm Ashwin. I'm a mathematician and entrepreneur.</p>
 
-<p>This website is under construction. In the meantime, these links might be useful:</p>
+  <p>This website is under construction. In the meantime, these might be useful:</p>
 
-<div>
-  <ul>
-    <li>Email: ashwinm@berkeley.edu</li>
-    <li>Twitter: @shwin_m</li>
-  </ul>
+  <div>
+    <ul>
+      <li>Email: ashwinm@berkeley.edu</li>
+      <li>Twitter: @shwin_m</li>
+    </ul>
+  </div>
 </div>
