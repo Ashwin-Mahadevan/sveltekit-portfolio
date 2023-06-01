a43c759cd12d514cb02e5a19c9cbbcd3b505fab8
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-01 19:08:35 -0400

    Profile Picture

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-  import "../app.css";
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
+	import "../app.css";
+	import { dev } from "$app/environment";
+	import { inject } from "@vercel/analytics";
+	import profile from "$lib/images/profile.png";
 
-  inject({ mode: dev ? "development" : "production" });
+	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
-  <title>Ashwin Mahadevan</title>
+	<title>Ashwin Mahadevan</title>
+	<link rel="icon" href={profile} />
 </svelte:head>
 
 <div class="container mx-auto">
-  <slot />
+	<slot />
 </div>
