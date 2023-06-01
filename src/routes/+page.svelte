d0fbe2be479819739661d6f2e9735c89a743f79b
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-01 19:07:31 -0400

    Formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-  const links = [
-    { name: "Website", content: "ashwinm.com", href: "https://ashwinm.com" },
-    { name: "Email", content: "ashwinm@berkeley.edu", href: "mailto:ashwinm@berkeley.edu" }
-  ];
+	const links = [
+		{ name: "Website", content: "ashwinm.com", href: "https://ashwinm.com" },
+		{ name: "Email", content: "ashwinm@berkeley.edu", href: "mailto:ashwinm@berkeley.edu" }
+	];
 </script>
 
 <div class="my-8">
-  <h1 class="text-5xl font-bold text-center">Ashwin Mahadevan</h1>
+	<h1 class="text-5xl font-bold text-center">Ashwin Mahadevan</h1>
 
-  <ul class="flex justify-center">
-    {#each links as { name, content, href }, index}
-      <!-- TODO: Link styling. -->
-      <li>{name}: <a {href} class="text-blue-700 underline"> {content} </a></li>
-      {#if index < links.length - 1} <span class="mx-2"> | </span> {/if}
-    {/each}
-  </ul>
+	<ul class="flex justify-center">
+		{#each links as { name, content, href }, index}
+			<!-- TODO: Link styling. -->
+			<li>{name}: <a {href} class="text-blue-700 underline"> {content} </a></li>
+			{#if index < links.length - 1} <span class="mx-2"> | </span> {/if}
+		{/each}
+	</ul>
 
-  <hr class="m-2" />
+	<hr class="m-2" />
 
-  <p class="text-center">Hi, I'm Ashwin. I'm a mathematician and entrepreneur.</p>
-  <p class="text-center">
-    <a href="https://www.spinster.ai/ashwinm" class="text-blue-700 underline">
-      I've Turned Into an Artificial Intelligence.
-    </a>
-  </p>
+	<p class="text-center">Hi, I'm Ashwin. I'm a mathematician and entrepreneur.</p>
+	<p class="text-center">
+		<a href="https://www.spinster.ai/ashwinm" class="text-blue-700 underline">
+			I've Turned Into an Artificial Intelligence.
+		</a>
+	</p>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 const config = {
-  preprocess: [vitePreprocess(), preprocess({ postcss: true })],
-  kit: { adapter: adapter({ runtime: "edge" }) }
+	preprocess: [vitePreprocess(), preprocess({ postcss: true })],
+	kit: { adapter: adapter({ runtime: "edge" }) }
 };
 
 export default config;
