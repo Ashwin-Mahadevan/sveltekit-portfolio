703171d1e3f7638e8d068e3cc327846c30ef5d17
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-02 17:12:56 -0400

    Static image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,13 @@
 	import "../app.css";
 	import { dev } from "$app/environment";
 	import { inject } from "@vercel/analytics";
-	import profile from "$lib/images/profile.png";
 
 	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
 	<title>Ashwin Mahadevan</title>
-	<link rel="icon" href={profile} />
+	<link rel="icon" href="/images/profile.png" />
 </svelte:head>
 
 <div class="container mx-auto">
