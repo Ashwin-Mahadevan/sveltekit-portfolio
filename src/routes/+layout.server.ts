d5ad3601122486f0c9fe41c2f8340106d040a468
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-06 10:42:18 -0400

    View tracking bugfix

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,3 @@
 import kv from "@vercel/kv";
-import { KV_REST_API_TOKEN, KV_REST_API_URL } from "$env/static/private";
 
-export const load = async () => ({
-	views: kv.get("views").then((views) => (views as number) || 0)
-});
+export const load = () => ({ views: kv.incr("views") });
