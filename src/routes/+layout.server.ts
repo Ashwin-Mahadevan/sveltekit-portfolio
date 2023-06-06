0a553a9611b9769eb59dc83dcde9caaed1e604c9
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-06 10:50:11 -0400

    Logging

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,10 @@ import kv from "@vercel/kv";
 
 const { format } = Intl.NumberFormat("en", { notation: "compact" });
 
-export const load = () => {
+export const load = async () => {
 	const views = kv.incr("views").then(format);
+
+	console.log(await views);
 
 	return {
 		stream: { views }
