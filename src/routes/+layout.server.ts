d1ce759053539edc8656bc258cb7da0fccc5f573
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-06-06 10:51:41 -0400

    Log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ const { format } = Intl.NumberFormat("en", { notation: "compact" });
 export const load = async () => {
 	const views = kv.incr("views").then(format);
 
-	console.log(await views);
+	console.log(typeof (await views));
 
 	return {
 		stream: { views }
