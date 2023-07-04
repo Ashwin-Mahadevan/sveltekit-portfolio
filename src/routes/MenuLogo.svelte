8b1c55231fcedbcfaea00edcf4313b009ca8f696
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-04 18:46:00 -0400

    MenuLogo component

---
+++ src/routes/MenuLogo.svelte
@@ -0,0 +1,8 @@
+<a href="/" class="p-2">
+	<span class="sr-only">Home</span>
+	<img
+		class="h-10 w-10 rounded-full ring ring-indigo-500"
+		src="/images/profile.png"
+		alt="Ashwin Mahadevan"
+	/>
+</a>
