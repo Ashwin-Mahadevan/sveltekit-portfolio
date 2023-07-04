972d8ff9953b03618aa4c8bc51180ecbbd62e61f
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-04 18:45:34 -0400

    Removed server-side javascript

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redis } from "$lib/server/database";
-
-const { format } = Intl.NumberFormat("en", { notation: "compact" });
-
-export const load = async () => {
-	const views = await redis.incr("views").then(format);
-
-	return { views };
-};
