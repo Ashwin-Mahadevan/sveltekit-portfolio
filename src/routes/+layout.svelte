9d9257a516664e3bd046020a7a8af7d99bdd4889
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-05 13:40:31 -0400

    Factored out MenuLinks into component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import posthog from "posthog-js";
 	import { PUBLIC_POSTHOG_API_KEY } from "$env/static/public";
 	import MenuLogo from "./MenuLogo.svelte";
+	import MenuLink from "./MenuLink.svelte";
 
 	const posthogConfig = { api_host: "https://app.posthog.com" };
 	if (browser) posthog.init(PUBLIC_POSTHOG_API_KEY, posthogConfig);
@@ -25,59 +26,32 @@
 </svelte:head>
 
 <header class="bg-white">
-	<nav class="mx-auto max-w-7xl flex items-center justify-between p-6" aria-label="Global">
+	<nav class="mx-auto max-w-7xl flex items-center justify-between p-6">
 		<div class="flex lg:flex-1 items-center space-x-4">
 			<MenuLogo />
-
-			{#if $page.url.pathname === "/"}
-				<h2 class="font-semibold text-gray-900 py-2 border-b-2">
-					<a href="/" class="hover:text-indigo-700">Ashwin Mahadevan</a>
-				</h2>
-			{:else}
-				<h2 class="font-semibold text-gray-900">
-					<a href="/" class="hover:text-indigo-700">Ashwin Mahadevan</a>
-				</h2>
-			{/if}
+			<MenuLink text="Ashwin Mahadevan" href="/" active={$page.url.pathname === "/"} />
 		</div>
 
-		<div class:hidden={showMenu} class="flex lg:hidden">
-			<button
-				on:click={() => (showMenu = true)}
-				class="inline-flex items-center justify-center rounded-md p-2 text-gray-700"
+		<button on:click={() => (showMenu = true)} class:hidden={showMenu} class="lg:hidden p-2">
+			<svg
+				class="h-8 w-8 text-gray-700"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				aria-hidden="true"
 			>
-				<span class="sr-only">Open main menu</span>
-				<svg
-					class="h-8 w-8"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					aria-hidden="true"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-					/>
-				</svg>
-			</button>
-		</div>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				/>
+			</svg>
+		</button>
 
 		<div class="hidden lg:flex space-x-12">
 			{#each menuLinks as { text, href }}
-				{#if $page.url.pathname === href}
-					<h2 class="text-md font-semibold text-gray-900 py-2 border-b-2">
-						<a {href} class="hover:text-indigo-700">
-							{text}
-						</a>
-					</h2>
-				{:else}
-					<h2 class="text-md font-semibold text-gray-900 py-2">
-						<a {href} class="hover:text-indigo-700">
-							{text}
-						</a>
-					</h2>
-				{/if}
+				<MenuLink {text} {href} active={$page.url.pathname === href} />
 			{/each}
 		</div>
 
@@ -90,28 +64,19 @@
 	<div class:hidden={!showMenu} class="lg:hidden" role="dialog" aria-modal="true">
 		<!-- Background backdrop, show/hide based on slide-over state. -->
 		<div class="fixed inset-0 z-10" />
+
 		<div
 			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
 		>
 			<div class="flex items-center justify-between">
 				<div class="flex items-center space-x-4">
 					<MenuLogo />
-
-					{#if $page.url.pathname === "/"}
-						<h2 class="font-semibold text-gray-900 py-2 border-b-2">
-							<a href="/" class="hover:text-indigo-700">Ashwin Mahadevan</a>
-						</h2>
-					{:else}
-						<h2 class="font-semibold text-gray-900">
-							<a href="/" class="hover:text-indigo-700">Ashwin Mahadevan</a>
-						</h2>
-					{/if}
+					<MenuLink text="Ashwin Mahadevan" href="/" active={$page.url.pathname === "/"} />
 				</div>
 
-				<button on:click={() => (showMenu = false)} class="rounded-md p-2 text-gray-700">
-					<span class="sr-only">Close menu</span>
+				<button on:click={() => (showMenu = false)} class="p-2">
 					<svg
-						class="h-8 w-8"
+						class="h-8 w-8 text-gray-700"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke-width="1.5"
@@ -123,29 +88,15 @@
 				</button>
 			</div>
 
-			<div class="mt-10">
-				<div class="space-y-4">
-					{#each menuLinks as { text, href }}
-						{#if $page.url.pathname === href}
-							<h2 class="text-md font-semibold text-gray-900 py-2 border-b-2">
-								<a {href} class="hover:text-indigo-700">
-									{text}
-								</a>
-							</h2>
-						{:else}
-							<h2 class="text-md font-semibold text-gray-900 py-2">
-								<a {href} class="hover:text-indigo-700">
-									{text}
-								</a>
-							</h2>
-						{/if}
-					{/each}
-				</div>
+			<div class="mt-4 space-y-4">
+				{#each menuLinks as { text, href }}
+					<MenuLink {text} {href} active={$page.url.pathname === href} />
+				{/each}
 			</div>
 		</div>
 	</div>
 </header>
 
-<div class="h-screen bg-gray-50">
+<div>
 	<slot />
 </div>
---
+++ src/routes/MenuLink.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let active = false;
+
+	export let text: string;
+	export let href: string;
+</script>
+
+{#if active}
+	<h2 class="text-md font-semibold text-gray-900 py-2 border-b-2">
+		<a {href} class="hover:text-indigo-700">
+			{text}
+		</a>
+	</h2>
+{:else}
+	<h2 class="font-semibold text-gray-900">
+		<a {href} class="hover:text-indigo-700">
+			{text}
+		</a>
+	</h2>
+{/if}
