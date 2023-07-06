eda16c119db0118a135b148790b2d0f79d4f5adf
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-06 14:21:12 -0400

    Blog Skeleton

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,63 @@
-This page is under construction. Please check back later.
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+	<div class="mx-auto max-w-2xl lg:max-w-4xl space-y-20">
+		<div class="space-y-2">
+			<h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">My Writing</h2>
+			<p class="text-lg leading-8 text-gray-600">
+				I've been collecting my thoughts in writing over the past two years. Initially, I decided
+				against releasing them publicly, but I've since changed my mind. I'll be posting here every
+				once in a while, so stay tuned!
+			</p>
+		</div>
+
+		<div class="space-y-20">
+			<article class="flex flex-col gap-8 lg:flex-row">
+				<img
+					src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+					alt=""
+					class="aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0 rounded-2xl"
+				/>
+
+				<div>
+					<div class="flex items-center gap-x-4 text-xs">
+						<time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+					</div>
+					<div class="group relative max-w-xl">
+						<h3
+							class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+						>
+							<a href="#">
+								<span class="absolute inset-0" />
+								Boost your conversion rate
+							</a>
+						</h3>
+						<p class="mt-5 text-sm leading-6 text-gray-600">
+							Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non
+							aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla
+							deserunt vel iusto corrupti dicta laboris incididunt.
+						</p>
+					</div>
+					<div class="mt-6 flex border-t border-gray-900/5 pt-6">
+						<div class="relative flex items-center gap-x-4">
+							<img
+								src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+								alt=""
+								class="h-10 w-10 rounded-full bg-gray-50"
+							/>
+							<div class="text-sm leading-6">
+								<p class="font-semibold text-gray-900">
+									<a href="#">
+										<span class="absolute inset-0" />
+										Michael Foster
+									</a>
+								</p>
+								<p class="text-gray-600">Co-Founder / CTO</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+
+			<!-- More posts... -->
+		</div>
+	</div>
+</div>
