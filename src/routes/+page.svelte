b550ead537ac66519374883bfe5d1797e6769002
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-05 23:50:01 -0400

    Homepage styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,38 @@
-Hello, World!
+<div class="mx-auto max-w-7xl lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-0">
+	<div class="px-6 lg:px-0 lg:pt-4 mx-auto max-w-2lg space-y-6 my-auto">
+		<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Nice to meet you!</h1>
+
+		<p class="text-lg leading-8 text-gray-600">
+			I'm a mathematicican and software engineer currently based in the San Francisco Bay Area.
+			Welcome to my website!
+		</p>
+
+		<a href="https://spinster.ai/@mahadevan" class="flex space-x-6">
+			<div class="rounded-full px-4 py-1 bg-indigo-600/10 border border-indigo-600/10">
+				<span class="text-sm font-semibold text-indigo-600">
+					I've turned into an artificial intelligence!
+				</span>
+			</div>
+
+			<div class="inline-flex items-center space-x-1">
+				<span class=" text-sm font-medium text-gray-600">Check it out</span>
+				<svg
+					class="h-5 w-5 text-gray-400"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+					/>
+				</svg>
+			</div>
+		</a>
+	</div>
+
+	<div class="mt-20 mx-auto md:max-w-2xl lg:mx-0 lg:mt-0">
+		<img src="/images/splash.png" class="shadow-lg rounded-xl" alt="Ashwin Mahadevan" />
+	</div>
+</div>
