72f701f283b34b1f1b440dba9cd2651a7cc6ae74
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-07-31 16:00:55 -0400

    Svelte config type

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import sveltePreprocess from "svelte-preprocess";
 
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
 	preprocess: [vitePreprocess(), sveltePreprocess({ postcss: true })],
 	kit: { adapter: adapter({ runtime: "edge", regions: "all" }) },
